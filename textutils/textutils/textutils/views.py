b4986e5b0a0ed26f860f69ea710d35ba76594cef
Sandeep Pushp <33807748+Sandeeppushp@users.noreply.github.com>
2019-04-29 22:58:28 +0530

    Add files via upload

---
+++ textutils/textutils/textutils/views.py
@@ -0,0 +1,73 @@
+# I have created this File - Sandeep
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request,'index.html')
+
+
+def analyze(request):
+    #Get the text
+    djtext = request.POST.get('text', 'default')
+    print(djtext)
+
+    # Check checkbox values
+    removepunc = request.POST.get('removepunc', 'off')
+    fullcaps = request.POST.get('fullcaps', 'off')
+    newlineremover = request.POST.get('newlineremover', 'off')
+    extraspaceremover = request.POST.get('extraspaceremover', 'off')
+    charactercounter = request.POST.get('charactercounter', 'off')
+
+    #Check which checkbox is on
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+
+        params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
+        djtext=analyzed
+
+    elif(fullcaps=="on"):
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+
+        params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+        djtext=analyzed
+
+    elif(extraspaceremover=="on"):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1]==" "):
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        djtext=analyzed
+
+    elif (newlineremover == "on"):
+        analyzed = ""
+        for char in djtext:
+            if char != "\n" and char!="\r":
+                analyzed = analyzed + char
+            else:
+                print("no")
+        print("pre", analyzed)
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        print(params)
+        djtext=analyzed
+
+    elif(charactercounter=="on"):
+        analyzed=len(djtext)
+        params = {'purpose': 'Total Characters', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
+
+    else:
+        return HttpResponse("Error")
+
+    return render(request,'analyze.html',params)
+
+
+
